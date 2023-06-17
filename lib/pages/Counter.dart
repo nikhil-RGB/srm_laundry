b66d2d84d8beb0302306526cba9d08df0b3567d8
Nikhil Narayanan <bleach.ichigo.anime@gmail.com>
2023-06-17 16:32:15 +0530

    ✨ Implement basic counter widget

---
+++ lib/pages/Counter.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+class Counter extends StatefulWidget {
+  final String title;
+  const Counter({super.key, required this.title});
+  @override
+  // ignore: library_private_types_in_public_api
+  _CounterState createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      if (_counter > 0) {
+        _counter--;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          widget.title,
+          style: TextStyle(fontSize: 16),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Container(
+          margin: const EdgeInsets.all(15.0),
+          height: 48.0,
+          decoration: BoxDecoration(
+            border: Border.all(width: 3, color: const Color(0xFF69B7FF)),
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              InkWell(
+                onTap: _decrementCounter,
+                child: Container(
+                  color: const Color(0xFF69B7FF),
+                  width: 48,
+                  alignment: Alignment.center,
+                  child: const Icon(
+                    Icons.remove,
+                    size: 24.0,
+                  ),
+                ),
+              ),
+              Text(
+                '$_counter',
+                style: const TextStyle(fontSize: 22.0),
+              ),
+              InkWell(
+                onTap: _incrementCounter,
+                child: Container(
+                  color: const Color(0xFF69B7FF),
+                  width: 48.0,
+                  alignment: Alignment.center,
+                  child: const Icon(
+                    Icons.add,
+                    size: 24.0,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/pages/CounterPage.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import 'Counter.dart';
+
+class CounterPage extends StatefulWidget {
+  const CounterPage({super.key});
+
+  @override
+  State<CounterPage> createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          SizedBox(
+            height: 40,
+          ),
+          Counter(
+            title: "Shirts",
+          ),
+        ],
+      ),
+    );
+  }
+}
